c679fda1ae294f29ff1c33e22419426b5f94cd18
sametkrt <omersametk@gmail.com>
2022-11-02 21:42:49 +0000

    fix

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
     # count = 1
     ami = var.ec2_ami_id
     instance_type = var.ec2_instance_type
+    instance_name = var.ec2_instance_name
     subnet_id = var.subnet_id
 
     tags = {
