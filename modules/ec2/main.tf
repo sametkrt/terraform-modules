feab4f80945a443d3da47d42d1091d1a92df8d6d
sametkrt <omersametk@gmail.com>
2022-11-02 21:46:52 +0000

    fix

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,8 @@
-resource "aws_instance" "web" {
-    # count = 1
-    ami = var.ec2_ami_id
-    instance_type = var.ec2_instance_type
-    instance_name = var.ec2_instance_name
-    subnet_id = var.subnet_id
+resource "aws_instance" "sample" {
+  ami           = var.ec2_ami_id
+  instance_type = var.ec2_instance_type
 
-    tags = {
-        Name = "WebServer"
-    }
+  tags = {
+    Name = var.ec2_instance_name
+  }
 }
--- modules/ec2/vars.tf
+++ modules/ec2/vars.tf
@@ -1,20 +1,15 @@
-variable "ec2_ami_id_temp" {
-  default = "ami-0c3fb0f6023840bc0"
-}
-
 variable "ec2_instance_type" {
-  default = "t2.micro" 
-}
-
-variable "ec2_instance_tenancy" {
-  default = "default"
-}
-
-variable "subnet_id" {
-  default = ""
+  type        = string
+  default     = "t3.micro"
+  description = "This is the AWS EC2 instance type / size to use."
 }
 
 variable "ec2_instance_name" {
   type        = string
   description = "The name to give the instance."
+}
+
+variable "ec2_ami_id" {
+  type        = string
+  description = "The AMI ID to use to launch the instance. NB: These differ between regions."
 }
